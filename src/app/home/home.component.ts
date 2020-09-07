777fc700c3d620fd8b03905a24310202fa73ca06
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-07 14:55:42 -0700

    Add toggle of h1 style on click of the button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+ 
+  firstClick(): void {
+    this.h1Style = !this.h1Style;
   }
 
 }
